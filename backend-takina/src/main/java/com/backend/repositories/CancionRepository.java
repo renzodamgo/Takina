aaf3a009a13d81c9447800a0f48220c3bb437f8c
Renzo Damian <renzo.damian.go@.com>
2021-04-28 18:13:07 -0500

    [fix] CancionRepository Annotation

--- backend-takina/src/main/java/com/backend/repositories/CancionRepository.java
+++ backend-takina/src/main/java/com/backend/repositories/CancionRepository.java
@@ -2,9 +2,11 @@ package com.backend.repositories;
 
 import com.backend.entities.Cancion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CancionRepository extends JpaRepository<Cancion,Long> {
 
     Optional<Cancion> findById(Long id);
