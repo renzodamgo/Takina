a5b4e1200b58dd4a07e254a6fe5bf461ff919d63
Renzo Damian <renzo.damian.go@.com>
2021-04-28 18:11:38 -0500

    Create CancionRepository

--- backend-takina/src/main/java/com/backend/repositories/CancionRepository.java
+++ backend-takina/src/main/java/com/backend/repositories/CancionRepository.java
@@ -1,12 +1,12 @@
-//package com.backend.repositories;
-//
-//import com.backend.entities.Cancion;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface CancionRepository extends JpaRepository<Cancion,Long> {
-//
-//    Optional<Cancion> findById(Long id);
-//    Optional<C>
-//}
+package com.backend.repositories;
+
+import com.backend.entities.Cancion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CancionRepository extends JpaRepository<Cancion,Long> {
+
+    Optional<Cancion> findById(Long id);
+
+}
