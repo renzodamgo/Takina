f7e50d7e587ccf0f6562799fa3f55dcbe2a77004
renxzen <u201415624@upc.edu.pe>
2021-04-28 12:59:16 -0500

    dtos y services

--- backend-takina/src/main/java/com/backend/dtos/ProyectoMusicalDto.java
+++ backend-takina/src/main/java/com/backend/dtos/ProyectoMusicalDto.java
@@ -1,12 +1,19 @@
 package com.backend.dtos;
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProyectoMusicalDto {
-    private Long id;
     private String nombre;
-    private String tipoProyecto;
+	private Float duracion;
+	private String descripcion;
+	private LocalDateTime fecha;
+	private Integer canciones;
+	private String discografica;
+	private Long artista_id;
+	
 }
