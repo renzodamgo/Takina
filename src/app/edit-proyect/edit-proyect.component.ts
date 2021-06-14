ff63f1cd39d55250f3502dc6d2194cb252e48123
Francesco <francesco8bassino@gmail.com>
2021-06-14 15:47:32 -0500

    edit proyecto en proceso

--- src/app/edit-proyect/edit-proyect.component.ts
+++ src/app/edit-proyect/edit-proyect.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProyectoService } from '../proyecto.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-edit-proyect',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProyectComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
--- src/app/proyects/proyects.component.ts
+++ src/app/proyects/proyects.component.ts
@@ -34,7 +34,6 @@ export class ProyectsComponent implements OnInit {
         
       })
     
-    
   }
 
 }
