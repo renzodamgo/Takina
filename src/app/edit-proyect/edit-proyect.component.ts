d83f446511318bab833a3c25043652fae97a487e
Renzo Damian <renzo.damian.go@gmail.com>
2021-06-18 17:50:36 -0500

    [Feat] show songs

--- src/app/edit-proyect/edit-proyect.component.ts
+++ src/app/edit-proyect/edit-proyect.component.ts
@@ -2,26 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProyectoService } from '../proyecto.service';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { Proyecto } from '../models/projecto';
+import { Canciones, Proyecto } from '../models/projecto';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-edit-proyect',
@@ -34,10 +16,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class EditProyectComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['track', 'nombre', 'duracion'];
+  
   project!:Proyecto;
-  name:string = "Bue"
+  canciones:Canciones[] = [];
+  
   id!:number
 
   constructor(
@@ -59,7 +42,8 @@ export class EditProyectComponent implements OnInit {
     this.proyectoService.getProyectoById(this.id)
       .subscribe((result)=>{
         this.project = result;
-        console.log(result)
+        this.canciones = result.canciones;
+        console.log(this.canciones)
       })
   }
 
